a52d50498c9030230e13f491e76d80cb67796b9f
Yii Kuo Chong <byu1628@proton.me>
2023-03-08 13:43:51 +0800

    Added recharts, and wrote code to fetch weather data and display them in a chart

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,67 @@
-import React from 'react';
-import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import React, { useState, useEffect } from 'react';
 import { Layout, Menu, theme } from 'antd';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis} from 'recharts';
 import "antd/dist/reset.css";
 import styles from '@/styles/Home.module.css';
 
 const { Header, Sider, Content, Footer } = Layout;
 
-export default function Home() {
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
+const Chart = () => {
+	const [data, setData] = useState(null)
+	const [isLoading, setLoading] = useState(false)
 
-  return (
-    <Layout>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={(broken) => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-      >
-        <div className={styles.logo} />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['4']}
-          items={[UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
-            (icon, index) => ({
-              key: String(index + 1),
-              icon: React.createElement(icon),
-              label: `nav ${index + 1}`,
-            }),
-          )}
-        />
-      </Sider>
-      <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }} />
-        <Content style={{ margin: '24px 16px 0' }}>
-          <div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>content</div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
-      </Layout>
-    </Layout>
-  )
+	useEffect(() => {
+		setLoading(true)
+		fetch('https://archive-api.open-meteo.com/v1/archive?latitude=4.30&longitude=101.15&start_date=2023-02-03&end_date=2023-03-03&daily=temperature_2m_mean&timezone=Asia%2FSingapore')
+			.then((res) => res.json())
+			.then((data) => {
+				const usable_data = data['daily']['time'].map((time, index) => ({ time: time, temperature: data['daily']['temperature_2m_mean'][index] }));
+	
+				setData(usable_data)
+				setLoading(false)
+			})
+		}, [])
+
+	if (isLoading) return <p>Loading...</p>
+	if (!data) return <p>No profile data</p>
+	console.log(data);
+	return (
+		<LineChart
+			width={1300}
+			height={579}
+			data={data}
+		>
+			<Line type="monotone" dataKey="temperature" stroke="#8884d8" />
+			<CartesianGrid stroke="#ccc" />
+			<XAxis dataKey="time" />
+			<YAxis />
+		</LineChart>
+	)
 }
+
+const Home = () => (
+	<Layout>
+		<Sider
+			breakpoint="lg"
+			collapsedWidth="0"
+			onBreakpoint={(broken) => {
+			console.log(broken);
+			}}
+			onCollapse={(collapsed, type) => {
+			console.log(collapsed, type);
+			}}
+		>
+			<div className={styles.logo} />
+			<Menu/>
+		</Sider>
+		<Layout>
+			<Header style={{ padding: 0 }}>DVisual</Header>
+			<Content style={{ margin: '24px 16px 0' }}>
+				<Chart />
+			</Content>
+			<Footer style={{ textAlign: 'center' }}>©2023 Created by Yii Kuo Chong with ❤️</Footer>
+		</Layout>
+	</Layout>
+)
+
+export default Home;
