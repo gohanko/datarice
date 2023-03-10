8024cd52299c0f68a86c1998e5ca8434a6a7840a
Yii Kuo Chong <byu1628@proton.me>
2023-03-10 11:46:21 +0800

    Overhaul the UI to make use of Ant.Design components and simplify how users load new data

--- pages/index.js
+++ pages/index.js
@@ -2,103 +2,46 @@ import React, { useState, useEffect } from 'react';
 import {
     Layout,
     Menu,
+    Card,
     theme,
     DatePicker,
     Form,
     Spin,
     Input,
     Button,
+    Radio,
 } from 'antd';
-import { RadarChartOutlined } from '@ant-design/icons';
+import {
+    RadarChartOutlined,  
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+} from '@ant-design/icons';
 import dayjs from 'dayjs';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, ResponsiveContainer, Tooltip } from 'recharts';
 
 import styles from '@/styles/Home.module.css';
 
-const { Header, Sider, Content } = Layout;
+const { Header, Sider, Content, Footer } = Layout;
 const { RangePicker } = DatePicker;
-
-const date_format = 'YYYY-MM-DD';
-
-const ChartSidebar = (props) => {
-    const handleOnFinish = (values) => {
-        props.submitCallback(values.latitude, values.longitude, values.data_range[0].format(date_format), values.data_range[1].format(date_format));
-    }
-
-    const disabledDate = (current) => {
-        return current && current > dayjs().endOf('day');
-    }
-
-    return (
-        <Sider
-            theme='light'
-            breakpoint="lg"
-            width={240}
-            onBreakpoint={(broken) => {
-                console.log(broken);
-            }}
-            onCollapse={(collapsed, type) => {
-                console.log(collapsed, type);
-            }}
-        >
-            <h1 className={styles.sidebar_menu_header}>
-                <RadarChartOutlined style={{ fontSize: '24px' }} />
-                <span className={styles.sidebar_menu_header_title}>DVisual</span>
-            </h1>
-
-            <Form
-                style={{ padding: "10px" }}
-                layout="vertical"
-                onFinish={handleOnFinish}    
-            >
-                <Form.Item label="Latitude" name="latitude" rules={[{ required: true, message: 'Please input the latitude!' }]}>
-                    <Input />
-                </Form.Item>
-                <Form.Item label="Longitude" name="longitude" rules={[{ required: true, message: 'Please input the longitude!' }]}>
-                    <Input />
-                </Form.Item>
-                <Form.Item label="Date Range" name="data_range" rules={[{ required: true, message: 'Please input the date range!' }]}>
-                    <RangePicker disabledDate={disabledDate} />
-                </Form.Item>
-                <Form.Item>
-                    <Button type="primary" htmlType="submit" ghost={true} block>
-                        Submit
-                    </Button>
-                </Form.Item>
-            </Form>
-        </Sider>
-    );
-}
-
-const ChartDisplay = (props) => {
-    return (
-        <ResponsiveContainer width="100%" height="100%">
-            <LineChart data={props.data}>
-                <CartesianGrid stroke="#ccc" strokeDasharray="5 5"/>
-                <XAxis dataKey="time" />
-                <YAxis />
-                <Line type="monotone" dataKey="temperature" stroke="#8884d8" />
-                <Tooltip />
-            </LineChart>
-        </ResponsiveContainer>
-    )
-}
 
 const Chart = () => {
     const [data, setData] = useState(null);
     const [isLoading, setLoading] = useState(false);
+    const [size, setSize] = useState('last_month'); // default is 'middle'
 
     useEffect(() => {
-        fetchData('4.30', '101.15', '2023-02-03', '2023-03-03');
+        fetchData('4.30', '101.15', dayjs(dayjs()).subtract(dayjs(dayjs()).daysInMonth(), 'day'), dayjs().subtract(7, 'day'));
     }, [])
 
     const fetchData = (latitude, longitude, start_date, end_date) => {
+        start_date = dayjs(start_date).format('YYYY-MM-DD')
+        end_date =  dayjs(end_date).format('YYYY-MM-DD')
+
+
         fetch(`https://archive-api.open-meteo.com/v1/archive?latitude=${latitude}&longitude=${longitude}&start_date=${start_date}&end_date=${end_date}&daily=temperature_2m_mean&timezone=Asia%2FSingapore`)
             .then((res) => res.json())
             .then((data) => {
-                console.log(data);
-                const usable_data = data['daily']['time'].map((time, index) => ({ time: time, temperature: data['daily']['temperature_2m_mean'][index] }));
-	
+                const usable_data = data['daily']['time'].map((time, index) => ({ time: dayjs(time).format('MMM D'), temperature: data['daily']['temperature_2m_mean'][index] }));
                 setData(usable_data)
                 setLoading(false)
             })
@@ -108,21 +51,90 @@ const Chart = () => {
     if (!data) return <Spin tip="Loading..." size="large" />
 
     return (
-        <>
-            <ChartSidebar submitCallback={fetchData} />
-            <Layout style={{ height: "100vh" }}>
-                <Content style={{ margin: '24px 32px 0 0' }}>
-                    <ChartDisplay data={data}/>
-                </Content>
+        <Card title={'Latest Kampar Daily Average Temperature'}>
+            <div
+                style={{
+                    width: '100%',
+                    display: 'flow-root',
+                    marginBottom: '10px',
+                }}
+            >
+                <Radio.Group
+                    value={size}
+                    onChange={(e) => {
+                        let start_date = '';
+                        let end_date = '';
+
+                        const val = e.target.value;
+                        start_date = dayjs().subtract(7, 'day');
+                        if (val == 'last_2_week') {
+                            end_date = dayjs(start_date).subtract(14, 'day')
+                        } else if (val == 'last_month') {
+                            end_date = dayjs(start_date).subtract(dayjs(start_date).daysInMonth(), 'day')
+                        } else if (val == 'last_year') {
+                            end_date = dayjs(start_date).subtract(12, 'month')
+                        }
+
+
+                        fetchData('4.30', '101.15', end_date, start_date);
+                        setSize(e.target.value)
+                    }}
+                    style={{
+                        float: 'right'
+                    }}
+                >
+                    <Radio.Button value="last_2_week">Last 2 week</Radio.Button>
+                    <Radio.Button value="last_month">Last month</Radio.Button>
+                    <Radio.Button value="last_year">Last year</Radio.Button>
+                </Radio.Group>
+            </div>
+            <Layout style={{ height: 240 }}>
+                <ResponsiveContainer width="100%" height="100%">
+                    <LineChart data={data} style={{ backgroundColor: 'white' }}>
+                        <CartesianGrid stroke="#ccc" strokeDasharray="5 5"/>
+                        <XAxis dataKey="time" />
+                        <YAxis />
+                        <Line type="monotone" dataKey="temperature" stroke="#8884d8" />
+                        <Tooltip />
+                    </LineChart>
+                </ResponsiveContainer>
             </Layout>
-        </>
+        </Card>
     );
 }
 
 const Home = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer, colorTextLightSolid },
+      } = theme.useToken();
+
     return (
-        <Layout>
-            <Chart />
+        <Layout style={{ height: '100%' }}>
+            <Sider
+                breakpoint="lg"
+                trigger={null}
+                collapsible
+                collapsed={collapsed}
+            >
+                <div className={styles.logo} />
+            </Sider>
+            <Layout>
+                <Header style={{ padding: 0, background: colorBgContainer }}>
+                    {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                        className: styles.trigger,
+                        onClick: () => setCollapsed(!collapsed),
+                    })}
+                </Header>
+                <Layout style={{ overflow: 'scroll' }}>
+                    <Content style={{ margin: '24px 16px' }}>
+                        <Chart />
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>
+                        DVisual - Created by <a href="https://github.com/gohanko" target="_blank">Yii Kuo Chong</a> with ❤️
+                    </Footer>
+                </Layout>
+            </Layout>
         </Layout>
     )
 };
