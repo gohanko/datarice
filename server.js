53fae258e0e12eba776ee2ace320b0830066cfb2
Yii Kuo Chong <byu1628@proton.me>
2023-05-21 01:38:21 +0800

    pkg: compileable version of the project. it throws an MODULE_NOT_FOUND error for socket-io for some reason

--- server.js
+++ server.js
@@ -1,32 +1,18 @@
 const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
- 
+const nextConfig = require('./next.config');
+
+const port = parseInt(process.env.PORT, 10) || 3003;
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
-const port = 3000;
-// when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, hostname, port });
+const app = next({ dev, dir: __dirname, conf: nextConfig });
 const handle = app.getRequestHandler();
- 
+
 app.prepare().then(() => {
-    createServer(async (req, res) => {
-    try {
-        // Be sure to pass `true` as the second argument to `url.parse`.
-        // This tells it to parse the query portion of the URL.
-        const parsedUrl = parse(req.url, true);
-        await handle(req, res, parsedUrl);
-    } catch (err) {
-        console.error('Error occurred handling', req.url, err);
-        res.statusCode = 500;
-        res.end('internal server error');
-    }
-    })
-    .once('error', (err) => {
-        console.error(err);
-        process.exit(1);
-    })
-    .listen(port, () => {
-        console.log(`> Ready on http://${hostname}:${port}`);
+    createServer((req, res) =>
+        handle(req, res, parse(req.url, true).pathname),
+    ).listen(port, err => {
+        if (err) throw err;
+        console.log(`> Ready on http://localhost:${port}`);
     });
 });
